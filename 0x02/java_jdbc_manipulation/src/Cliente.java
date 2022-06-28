210be8176c28dfe29767a930141d066ec5cda02f
gabrielalichy <gabrielalichy@ciandt.com>
2022-06-28 10:34:23 -0300

    relacionaments entre classes e tabelas DB

---
+++ 0x02/java_jdbc_manipulation/src/Cliente.java
@@ -0,0 +1,39 @@
+public class Cliente {
+    private int id;
+    private String nome;
+    private int idade;
+    private String cpf;
+    private String RG;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getRG() {
+        return RG;
+    }
+    public void setRg(String RG) {
+        this.RG = RG;
+    }
+
+}
